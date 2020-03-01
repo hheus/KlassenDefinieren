d306ea7f037ed632a1bcaed1819a98d150ea83ea
Holger Heus <holger.heus@unitybox.de>
2020-03-01 21:02:21 +0100

    Projektdateien hinzufügen.

---
+++ Auto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlassenDefinieren
+{
+    public class Auto
+    {
+        public string marke;
+        public int alterInJahren;
+
+        public void BeschleinigeAuf100Kmh()
+        {
+            if (IstSchnell())
+            {
+                Console.WriteLine("Habe schnell beschleunigt");
+            }
+            else
+            {
+                Console.WriteLine("Habe normal beschleunigt");
+            }
+        }
+
+        private bool IstSchnell()
+        {
+            if (marke == "Porsche")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
